178f9d0472c9a9dbb123bb80a734b2e82b63590e
Akdom2001 <Akdom2001@2175b78e-1baa-11de-9c9a-2d325a3e6494>
2009-03-28 20:48:01 +0000

    Added tile rotations on click event

--- game.py
+++ game.py
@@ -15,7 +15,14 @@ NUM_LEMS = 10
 lemsAlive = 0
 TILE_WIDTH = 40
 
-
+def mouseControl():
+	for event in pygame.event.get():
+		if event.type == MOUSEBUTTONDOWN:
+			mousePos = pygame.mouse.get_pos()
+			for tile in tilegroup:
+				if (tile.position[0]-20 < mousePos[0] < tile.position[0]+20 \
+				and tile.position[1]-20 < mousePos[1] < tile.position[1]+20):
+					tile.rotate()
 
 class Lemming(pygame.sprite.Sprite):
     MAX_FORWARD_SPEED = 10
@@ -68,17 +75,29 @@ class Tower(pygame.sprite.Sprite):
 
 
 class Tile(pygame.sprite.Sprite):
-    def __init__(self, position, image="images/default-tile_8.gif"):
+    def __init__(self, position, image="images/default-tile_8.gif", orient="N"):
         pygame.sprite.Sprite.__init__(self)
         self.src_image = pygame.image.load(image)
         self.position = (position[0]*TILE_WIDTH + (TILE_WIDTH/2), \
             position[1]*TILE_WIDTH + (TILE_WIDTH/2))
         self.image = self.src_image
         self.rect = self.image.get_rect()
+        self.orient = { "N":0,
+            "S":180,
+            "E":90,
+            "W":270}[orient]
+        self.image = pygame.transform.rotate(self.image, self.orient)
 
     def update(self, deltaT):
         self.position = self.position
         self.rect.center = self.position
+	
+    def rotate(self):
+        self.image = pygame.transform.rotate(self.image, 90)
+        self.orient = self.orient + 90
+        if self.orient == 360:
+            self.orient = 0
+		
 
 # Define the random direction chooser
 random.seed(time.time())
@@ -91,7 +110,7 @@ tilegroup = pygame.sprite.RenderPlain()
 
 # Randomly create towers
 for i in range(0, 100):
-    towergroup.add(Tower((random.randint(0, 24), random.randint(0, 15))))
+    tilegroup.add(Tile((random.randint(0, 24), random.randint(0, 15)), "images/l.png"))
 
 
 def updateBoard():
@@ -129,3 +148,4 @@ while 1:
     updateBoard()
     detectCollisions()
     pygame.display.flip()
+    mouseControl() #Rotate tiles on mouse click
