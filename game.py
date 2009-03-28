528fd76dfe409ab51c0a8392ec1993c3a4d7678e
spiffytech <spiffytech@2175b78e-1baa-11de-9c9a-2d325a3e6494>
2009-03-28 16:52:34 +0000

    Made tower.png transparent

--- game.py
+++ game.py
@@ -3,7 +3,7 @@ from pygame.locals import *
 
 SCREEN_SIZE = (1024, 768)
 screen = pygame.display.set_mode(SCREEN_SIZE)
-BACKGROUND = (255, 248, 168)
+BACKGROUND = (0, 0, 0)
 clock = pygame.time.Clock()
 FRAMERATE = 30
 
@@ -15,7 +15,7 @@ class Lemming(pygame.sprite.Sprite):
     TURN_SPEED = 5
     ACCELERATION = 2
 
-    def __init__(self, position, image="lemming.png"):
+    def __init__(self, position, image="images/lemming.png"):
         pygame.sprite.Sprite.__init__(self)
         self.src_image = pygame.image.load(image)
         self.position = position
@@ -45,12 +45,33 @@ class Lemming(pygame.sprite.Sprite):
         self.rect.center = self.position
 
 
+class Tower(pygame.sprite.Sprite):
+    def __init__(self, position, image="images/tower1.png"):
+        pygame.sprite.Sprite.__init__(self)
+        self.src_image = pygame.image.load(image)
+        self.position = position
+        self.image = self.src_image
+        self.rect = self.image.get_rect()
+
+    def fire():
+        pass
+
+    def update(self, deltaT):
+        pass
+
+
+
 rect = screen.get_rect()
 lemming = Lemming((100,100))
 lgroup = pygame.sprite.RenderPlain(lemming)
+tower = Tower((100, 500))
+tgroup = pygame.sprite.RenderPlain(tower)
+
 while 1:
     deltaT = clock.tick(30)
-    screen.fill((0, 0, 0))
+    screen.fill(BACKGROUND)
     lgroup.update(deltaT)
+    tgroup.update(deltaT)
     lgroup.draw(screen)
+    tgroup.draw(screen)
     pygame.display.flip()
