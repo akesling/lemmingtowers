dc7f8190906e921b3564327c3baa3204da5fb663
spiffytech <spiffytech@2175b78e-1baa-11de-9c9a-2d325a3e6494>
2009-03-28 18:49:36 +0000

    Added collision detection for the lemmings + towers

--- trunk/game.py
+++ trunk/game.py
@@ -1,11 +1,17 @@
-import pygame, math, sys
+import math
+import pygame
 from pygame.locals import *
+import random
+import sys
+import time
 
+# Game settings
 SCREEN_SIZE = (1024, 768)
 screen = pygame.display.set_mode(SCREEN_SIZE)
 BACKGROUND = (0, 0, 0)
 clock = pygame.time.Clock()
 FRAMERATE = 30
+NUM_LEMS = 10
 
 
 
@@ -23,7 +29,7 @@ class Lemming(pygame.sprite.Sprite):
         self.k_left = self.k_right = self.k_up = self.k_down = 0
 
     def update(self, deltaT):
-        if self.position[0] == SCREEN_SIZE[0] or self.position[1] == SCREEN_SIZE[1]:
+        if self.position[0] == SCREEN_SIZE[0] or self.position[1] == SCREEN_SIZE[1] or self.position[0] == 0 or self.position[1] == 0:
             self.speed = 0
 
         self.speed += (self.k_up + self.k_down)
@@ -44,6 +50,7 @@ class Lemming(pygame.sprite.Sprite):
         self.rect.center = self.position
 
 
+
 class Tower(pygame.sprite.Sprite):
     def __init__(self, position, image="images/tower.png"):
         pygame.sprite.Sprite.__init__(self)
@@ -60,18 +67,31 @@ class Tower(pygame.sprite.Sprite):
         self.rect.center = self.position
 
 
+# Define the random direction chooser
+random.seed(time.time())
 
+# Define the towers/lemming that will be on the map
 rect = screen.get_rect()
-lemming = Lemming((100,100))
-lgroup = pygame.sprite.RenderPlain(lemming)
-tower = Tower((100, 200))
+lemming = Lemming((100, 100))  # Define a lemming
+lgroup = pygame.sprite.RenderPlain(lemming)  # Add it to the lemming group
+tower = Tower((100, 200))  # Same for towers
 tgroup = pygame.sprite.RenderPlain(tower)
 
+# Start the game loop
 while 1:
-    deltaT = clock.tick(30)
-    screen.fill(BACKGROUND)
-    lgroup.update(deltaT)
+    deltaT = clock.tick(30)  # Controll the framerate (which controls game speed)
+    screen.fill(BACKGROUND)  # Background color
+    lgroup.update(deltaT)  # Update the positions of our objects
     tgroup.update(deltaT)
+
+    # See if our lemming collided with anything
+    for l in lgroup:
+        if pygame.sprite.spritecollide(l, tgroup, False):  # Collision with a tower
+            l.direction += random.choice((90, -90))  # Rotate either left or right
+
+    # Redraw our object positions
     lgroup.draw(screen)
     tgroup.draw(screen)
+
+    # Redraw the map with the above changes
     pygame.display.flip()
