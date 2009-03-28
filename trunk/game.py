4de4da6dfd474831d9ebad465fe2f425d38c0183
spiffytech <spiffytech@2175b78e-1baa-11de-9c9a-2d325a3e6494>
2009-03-28 15:36:04 +0000

    A single lemming is now created, and moves across the screen

--- trunk/game.py
+++ trunk/game.py
@@ -3,7 +3,6 @@ from pygame.locals import *
 
 screen = pygame.display.set_mode((1024, 768))
 BACKGROUND = (255, 248, 168)
-lemming = pygame.image.load("car.png")
 clock = pygame.time.Clock()
 FRAMERATE = 30
 speed = direction = 0
@@ -11,8 +10,9 @@ position = (100, 100)
 
 
 
-class lemming(pygame.sprite.Sprite):
+class Lemming(pygame.sprite.Sprite):
     MAX_FORWARD_SPEED = 10
+    MAX_REVERSE_SPEED = 0
     TURN_SPEED = 5
     ACCELERATION = 2
 
@@ -20,11 +20,12 @@ class lemming(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         self.src_image = pygame.image.load(image)
         self.position = position
-        self.speed = self.direction = 0
+        self.speed = 2
+        self.direction = 0
         self.k_left = self.k_right = self.k_up = self.k_down = 0
 
     def update(self, deltaT):
-        self.speed += (k_up + k_down)
+        self.speed += (self.k_up + self.k_down)
         if self.speed > self.MAX_FORWARD_SPEED:
             self.speed = self.MAX_FORWARD_SPEED
         if self.speed < self.MAX_REVERSE_SPEED:
@@ -40,3 +41,14 @@ class lemming(pygame.sprite.Sprite):
         self.image = pygame.transform.rotate(self.src_image, self.direction)
         self.rect = self.image.get_rect()
         self.rect.center = self.position
+
+
+rect = screen.get_rect()
+lemming = Lemming(rect.center)
+lgroup = pygame.sprite.RenderPlain(lemming)
+while 1:
+    deltaT = clock.tick(30)
+    screen.fill((0, 0, 0))
+    lgroup.update(deltaT)
+    lgroup.draw(screen)
+    pygame.display.flip()
